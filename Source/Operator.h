484e302a05f7e9892d72d7e4f71391b1fc10d91f
Ryan Baker <rbaker301@gatech.edu>
2025-09-07 16:15:20 -0400

    fixed FM bugs - state leakage and freq calc

--- Source/Operator.h
+++ Source/Operator.h
@@ -50,7 +50,7 @@ private:
 	float lastSample = 0.f;
 	float cachedSample;
 	int opIndex;
-	float sampleRate, frequency, level, ratio, tuning;
+	float sampleRate, baseFrequency, level, ratio, tuning;
 	int note;
 	std::vector<Operator*> modOperators;
 	juce::SmoothedValue<float, juce::ValueSmoothingTypes::Multiplicative> freqSmooth; //multiplicative for frequency per juce docs
